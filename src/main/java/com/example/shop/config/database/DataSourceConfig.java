2b1a0877f6b7dc8976f4d33cb8e9be3d50423f2b
안형욱 <ahw9998@gmail.com>
2023-11-09 00:34:40 +0900

    init commit

---
+++ src/main/java/com/example/shop/config/database/DataSourceConfig.java
@@ -0,0 +1,55 @@
+package com.example.shop.config.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.example.shop.api"
+)
+public class DataSourceConfig {
+
+    @Bean("writeDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.write")
+    public DataSource writeDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean("readDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.read")
+    public DataSource readDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean("routingDataSource")
+    @ConditionalOnBean(name = {"writeDataSource", "readDataSource"})
+    public DataSource routingDataSource(
+        @Qualifier("writeDataSource") DataSource writeDataSource,
+        @Qualifier("readDataSource") DataSource readDataSource) {
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        routingDataSource.setTargetDataSources(
+            Map.of("write", writeDataSource, "read", readDataSource));
+        routingDataSource.setDefaultTargetDataSource(writeDataSource);
+        return routingDataSource;
+    }
+
+    @Bean(name = "currentDataSource")
+    @ConditionalOnBean(name = "routingDataSource")
+    public LazyConnectionDataSourceProxy currentDataSource(DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+
+}
---
+++ src/main/java/com/example/shop/config/database/RoutingDataSource.java
@@ -0,0 +1,12 @@
+package com.example.shop.config.database;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "read" : "write";
+    }
+}
